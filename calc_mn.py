dda7aeffc306e8b7109863c16b49fafc48e53472
mauricioBucerius <52835194+mauricioBucerius@users.noreply.github.com>
2023-03-15 14:41:44 +0100

    Update calc_mn.py
    Adding Fano Criteria and some additional calculations to resonate out a component

--- calc_mn.py
+++ calc_mn.py
@@ -4,12 +4,35 @@ import sys
 # sys.path.insert(0, 'K:\\05_Python\\converting-main')
 from prefix_converting import zahl2prefix
 
-Rs = 50     # source impendance
-Rl = 10     # load impendance
-f = 7.5e9   # carrier frequency
-B = 3e9     # bandwidth -> important for q
-MN = 'pi-network' # kind of matching network
 
+def fano_crit(Q_0, Q_L):
+    """
+    with the bode-fano criterium this gives the upper limitation about the best
+    possible reflection coefficient, which is with a matching network possible
+
+    Parameters
+    ----------
+    Q_0 : float
+        DESCRIPTION.
+    Q_L : float
+        loaded quality factor.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+ 
+    """
+    return np.exp(-pi*Q_0/Q_L)
+
+def get_Q_0(omega_0, delta_omega):
+    return omega_0/delta_omega
+
+def get_omega_0(f1, f2):
+    return np.sqrt((2*pi)**2 *f1 * f2)
+
+def get_delta_omega(f1, f2):
+    return np.abs( 2*pi*(f1 - f2) )
 
 def calc_r_par(q_p, r_ser):
     return (1 + q_p**2)*r_ser
@@ -17,16 +40,38 @@ def calc_r_par(q_p, r_ser):
 def calc_x_par(q_p, x_ser):
     return x_ser*(1 + 1/q_p**2)
 
-def print_comps(L_ser, C_par, C_ser, L_par):
-    print('Schaltung 1:')
-    print('------------')
-    print(f'L_ser = {zahl2prefix(L_ser)}H')
-    print(f'C_par = {zahl2prefix(C_par)}F\n')
-      
-    print('Schaltung 2:')
-    print('------------')
-    print(f'C_ser = {zahl2prefix(C_ser)}H')
-    print(f'L_par = {zahl2prefix(L_par)}F')
+def print_comps(**kwargs):
+    
+    for key, value in kwargs.items():
+        if key == 'L_ser':
+            if value > 0:
+                print(f'L_ser = {zahl2prefix(value)}H')
+            else:
+                print(f'C_ser = {zahl2prefix(-value)}F')
+                
+        elif key == 'C_par':
+            if value > 0:
+                print(f'C_par = {zahl2prefix(value)}F')
+            else:
+                print(f'L_par = {zahl2prefix(-value)}H')
+                
+        elif key == 'L_par':
+            if value > 0:
+                print(f'L_par = {zahl2prefix(value)}H')
+            else:
+                print(f'C_par = {zahl2prefix(-value)}F')
+            
+        elif key == 'C_ser':
+                if value > 0:
+                    print(f'C_ser = {zahl2prefix(value)}F')
+                else:
+                    print(f'L_ser = {zahl2prefix(-value)}H')
+    print()
+        
+    # print('Schaltung 2:')
+    # print('------------')
+    # print(f'C_ser = {zahl2prefix(C_ser)}H')
+    # print(f'L_par = {zahl2prefix(L_par)}F')
     
 def calc_q(r, **kwargs):
     # r = None
@@ -101,17 +146,18 @@ def get_l_network(r_l, r_s):
         q = calc_q(r_s, r_ser=r_l)
         x_ser = q*r_s       
         x_par = r_l/q
-        
+        Q = r_l/x_par
     else:
         # x_par is parallel to the source -> reduces the source and enlarges load
         q = calc_q(r_l, r_par=r_s)
         x_ser = q*r_l
         x_par = r_s/q
+        Q = r_s/x_par
     # print(q)
-    return x_ser, x_par
+    return x_ser, x_par, Q
 
 def get_pi_network(q, r_l, r_s):
-    r_vir = calc_r_vir(q, Rs, Rl)
+    r_vir = calc_r_vir(q, r_s, r_l)
     # print(r_vir)
     # 1. L-Network - close to the source
     x_ser1, x_par1 = get_l_network(r_vir, r_s)   # Matching from source to r_vir
@@ -121,51 +167,162 @@ def get_pi_network(q, r_l, r_s):
     # print(r_vir)
     return x_ser1, x_ser2, x_par1, x_par2
     
-def get_t_network():
-    pass
+def get_t_network(q, r_l, r_s):
+    r_vir = calc_r_vir(q, r_s, r_l)
+    
+    print(r_vir, q)
+    
+    # 1. L-Network - close to the source (series Xl1 to source)
+    X_ser1, x_par1 = get_l_network(r_l, r_s)
+    
+def calc_resonator(f_res, **kwargs):
+    # inline calculation of the component value
+    calc_comp = lambda f, comp: 1/(2*pi*f)**2 * 1/comp  
+    
+    C = None
+    L = None
+    
+    for key, value in kwargs.items():
+        if key == 'C':
+            C = value
+        elif key == 'L':
+            L = value
+    if C is None and L is None:
+        C = 1e-12
+        
+    if C is None and L is not None:
+        C = calc_comp(f_res, L)
+    elif L is None and C is not None:
+        L = calc_comp(f_res, C)
+        
+    return C, L
+
 
 def main():
-    q_tot = f/B     # carrier and bandwidth of the circuit determine the overall quality factor
+    Rs = 50             # source impendance
+    
+    # Größen des pHEMT bei 8 Finger und 125 um Gatebreite
+    R_phemt = 3.2                   # Ohm - Eingangswiderstand des pHEMT
+    C_phemt = 2.29e-12              # F - Eingangskapazität des pHEMT
+    
+    # R_phemt = 62.5                   # Ohm - Eingangswiderstand des pHEMT
+    # C_phemt = 34.8e-15         # F - Eingangskapazität des pHEMT
+    
+    f0 = 12e9            # carrier frequency
+    B = 6e9             # bandwidth -> important for q
+    MN = 'l-network'   # kind of matching network
+    N_HARM = 7          # Anzahl an Harmonics für den Class-F Amplifier
+    
+    f1 = f0 - B/2       # obere Grenzfrequenz
+    f2 = f0 + B/2       # untere Grenzfreqnez
+    q_tot = f0/B     # carrier and bandwidth of the circuit determine the overall quality factor
+    Z_l = R_phemt - 1j * 1 / (2 * pi *f0 * C_phemt)     # load impendance
+    
+    Z_l = 0.296 * 50
+    
+    # print(Z_l)
+    # get_t_network(q_tot, Rl, Rs)
+    
+    
+    Q_in = 1/(2*pi*f0*R_phemt*C_phemt)
+    omega_0 = get_omega_0(f1, f2)
+    delta_omega = get_delta_omega(f1, f2)
+    gamma_max = 10*np.log10(fano_crit(get_Q_0(omega_0, delta_omega), Q_in))
+    
+    print('--------------------------------------------------------------------\n')
+    print('Bode-Fano Criterion')
+    print(f'Gamma = {round(gamma_max,1)} dB \nB = {zahl2prefix(B)}Hz')
+    print(f'total Q = {round(q_tot,1)}')
+    print('--------------------------------------------------------------------')
+    
+    # C_f0, L_f0 = calc_resonator(f0, L=500e-12)
+    # C_f3, L_f3 = calc_resonator(3*f0, L=500e-12)
+    
+    # print(f'C = {zahl2prefix(C_f3)}F')
+    # print(f'L = {zahl2prefix(L_f3)}H')
     
     if MN == 'l-network':
-        # berechnet ein L-Netzwerk zur Anpassung
-        x_ser, x_par = get_l_network()
-        # Serielle Bauteile
-        L_ser = x_ser/(2*pi*f)
-        C_ser = 1/(2*pi*f*x_ser)
         
-        # parallele Bauteile
-        C_par = 1/(2*pi*f*x_par)
-        L_par = x_par/(2*pi*f)
-        print(f'Fehlanpassung wird mittels L-Netzwerk über Q = {zahl2prefix(q_tot)}kompensiert:\n')
-        print_comps(L_ser, C_par, C_ser, L_par)
+        if np.imag(Z_l) < 0:
+            L_res = -np.imag(Z_l)/(2*pi*f0)
+            X_L_res = 2*pi*f0*L_res
+            
+            print('Resonating the capacitor:')
+            print(f'L = {zahl2prefix(L_res)}H \n')
+        else:
+            X_L_res = 0
+            
+        # berechnet ein L-Netzwerk zur Anpassung
+        x_ser, x_par, q_calc = get_l_network(Rs, np.real(Z_l))
+        
+        if Rs > np.real(Z_l):
+            # parallel branch is up to the source
+            print(f'Source > Load - parallel to the Source')
+            if X_L_res > 0:
+                # Parallele Bauteile zur Source
+                C_par = 1/(2*pi*f0*x_par)
+                L_par = x_par/(2*pi*f0)
+                
+                # Serielle Spule/Kapazität zur Last
+                L_ser = (x_ser+X_L_res)/(2*pi*f0)
+        
+                res_x = X_L_res-x_ser
+                if res_x > 0:     
+                    C_ser = 1/(2*pi*f0*res_x)
+                else:
+                    C_ser = res_x/(2*pi*f0)
+            else:
+                L_ser = x_ser/(2*pi*f0)
+                C_ser = 1/(2*pi*f0*x_ser)
+                
+                # parallele Bauteile
+                C_par = 1/(2*pi*f0*x_par)
+                L_par = x_par/(2*pi*f0)
+
+        else: 
+            # parallel branch is up to the load      
+            L_ser = (X_L_res-x_ser)/(2*pi*f0)
+            C_ser = 1/(2*pi*f0*x_ser)
+            
+            C_par = 1/(2*pi*f0*x_par)
+            L_par = x_par/(2*pi*f0)
+        
+        print(f'Fehlanpassung wird mittels L-Netzwerk über Q = {round(q_calc,1)} kompensiert:\n')
+        print('Schaltung 1:')
+        print('------------')
+        print_comps(L_ser=L_ser, C_par=C_par)
+        
+        print('Schaltung 2:')
+        print('------------')
+        print_comps(C_ser=C_ser, L_par=L_par)
         
     else:
-        if Rs > Rl:
+        if Rs > Z_l:
             print('Source > Load -> Pi-Network')
             print('---------------------------')
             
-            x_ser1, x_ser2, x_par1, x_par2 = get_pi_network(q_tot, Rl, Rs)
+            x_ser1, x_ser2, x_par1, x_par2 = get_pi_network(q_tot, Z_l, Rs)
             
             # Calculating the components
-            C_par1 = 1/(2*pi*f*x_par1)
-            C_par2 = 1/(2*pi*f*x_par2)
-            L_ser = x_ser1/(2*pi*f) + x_ser2/(2*pi*f)
+            C_par1 = 1/(2*pi*f0*x_par1)
+            C_par2 = 1/(2*pi*f0*x_par2)
+            L_ser = x_ser1/(2*pi*f0) + x_ser2/(2*pi*f0)
             print('Circuit 1:')
             print(f'C_par1 = {zahl2prefix(C_par1)}F')
             print(f'L_ser = {zahl2prefix(L_ser)}H')
             print(f'C_par2 = {zahl2prefix(C_par2)}F')
             
-            L_par1 = x_par1/(2*pi*f)
-            L_par2 = x_par2/(2*pi*f)
-            C_ser = 1/((2*pi*f*x_ser1) + (2*pi*f*x_ser1))
+            L_par1 = x_par1/(2*pi*f0)
+            L_par2 = x_par2/(2*pi*f0)
+            C_ser = 1/((2*pi*f0*x_ser1) + (2*pi*f0*x_ser1))
             print('\nCircuit 2:')
             print(f'L_par1 = {zahl2prefix(L_par1)}H')
             print(f'C_ser = {zahl2prefix(C_ser)}F')
             print(f'L_par2 = {zahl2prefix(L_par2)}H')
-        elif Rs < Rl:
+        elif Rs < Z_l:
             print('Source < Load -> T-Network')
             print('---------------------------')
+        
         
 if __name__ == '__main__':
     main()
